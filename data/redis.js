acf78ef88b2ade159b37ca922ae9911e64c20187
Jake Reichert <jake@jakereichert.com>
2024-09-23 10:39:55 -0700

    moving things around
    Signed-off-by: Jake Reichert <jake@jakereichert.com>

---
+++ data/redis.js
@@ -0,0 +1,16 @@
+dotenv.config();
+
+import Redis from "ioredis";
+
+export const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+});
+
+redis.on("connect", () => {});
+
+redis.on("error", (error) => {
+  console.error("Redis connection error", { error });
+});
